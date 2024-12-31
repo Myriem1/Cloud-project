7763b4afcd410c7f59246d3561198eb514753ff9
Myriem1 <mariemaaskri@gmail.com>
2024-12-31 11:06:44 +0100

    Ajout dossier du projet

--- application-code/web-tier/src/components/Home/Home.js
+++ application-code/web-tier/src/components/Home/Home.js
@@ -1,6 +1,6 @@
 
     import React, {Component} from 'react';
-    import architecture from '../../assets/3TierArch.png'
+    import architecture from '../../assets/3TierArch1.png'
 
     class Home extends Component {
         render () {
